939677f02df28fa664f553da2cd6ab39f75222a7
IM-ABHIJEET <imabhijeet1974@gmail.com>
2025-02-27 13:54:22 +0530

    Create dictionaries_basics.py

---
+++ dictionaries_basics.py
@@ -0,0 +1,10 @@
+dictt ={}
+x=int(input("Enter physics : "))
+dictt.update({"physics":x})
+
+y=int(input("Enter chem : "))
+dictt.update({"chem":y})
+
+z=int(input("Enter maths: "))
+dictt.update({"maths":z})
+print(dictt)
